07ce6d1c164876b6db35a28055f289e2dcda105b
Ilona Bazyk <ilona.bazyk@gmail.com>
2017-10-06 10:11:03 +0300

    some changes in html and js

--- js/scripts.js
+++ js/scripts.js
@@ -69,16 +69,36 @@ $(document).ready(function(){
   return num
 }
 
-      function sendMinMaxTemp(ind){
+      function sendTempIcon(ind){
         $('.days').eq(0).find('.temp-max').text(getMaxTempToday(ind))
         $('.days').eq(0).find('.temp-min').text(getMinTempToday(ind))
       for (var i=0; i<4; i++){
         var dopInd = ind + 8*i
         $('.days').eq(i+1).find('.temp-max').text(getMaxTemp(dopInd))
         $('.days').eq(i+1).find('.temp-min').text(getMinTemp(dopInd))
+        $('.days').eq(i+1).find('img').attr('src', 'http://openweathermap.org/img/w/'+ geticon(dopInd) +'.png')
+       
        }
       }
-           sendMinMaxTemp(searchIndexNext())
+           sendTempIcon(searchIndexNext())
+           
+           function geticon(ind) {
+  var arr = []
+  for (var i=ind; i < ind+8;i++){
+      arr.push(data1.list[i].weather[0].icon)
+  }
+      arr.sort()
+      if (arr[7]=='01n'){
+        return '01d'
+      } else if (arr[7]=='02n'){
+        return '02d'
+        } else if (arr[7]=='10n'){
+        return '10d'
+      } else {
+  return arr[7]
+      }
+}
+      
 	});
 })
 
@@ -86,75 +106,75 @@ $(document).ready(function(){
 
 
 function arrDays(day){
-  var arr = ["пн", "вт", "ср", "чт", "пт", "сб", "нд"]
-  var arr1 = []
-  for (var i = arr.indexOf(day); i<7; i++){
-    arr1.push(arr[i])
-  }
-  for (var j = 0; j<arr.indexOf(day); j++){
-    arr1.push(arr[j])
-  }
-  return arr1
+	var arr = ["пн", "вт", "ср", "чт", "пт", "сб", "нд"]
+	var arr1 = []
+	for (var i = arr.indexOf(day); i<7; i++){
+		arr1.push(arr[i])
+	}
+	for (var j = 0; j<arr.indexOf(day); j++){
+		arr1.push(arr[j])
+	}
+	return arr1
 }
 
 function timestamp2date(timestamp) { 
-    var theDate = new Date(timestamp * 1000); 
-    return theDate.toGMTString(); 
+	var theDate = new Date(timestamp * 1000); 
+	return theDate.toGMTString(); 
 }
 
 function getfiveDays(today, month, nameday){
 	for (var i = 1; i < 5; i++) {
 		$('.days').eq(i).find('.date>span').text(Number(today) + Number(i) + ' ' + month)	
-        $('.days').eq(i).find('.day-name').text(arrDays(nameday)[i])
+		$('.days').eq(i).find('.day-name').text(arrDays(nameday)[i])
 	}		
 }
 
 function getDayOfWeek(number){
 	switch (number) {
-	case 'Mon':
+		case 'Mon':
 		return 'пн';
-	case 'Tue':
+		case 'Tue':
 		return 'вт';
-	case 'Wed':
+		case 'Wed':
 		return 'ср'
-	case 'Thu':
+		case 'Thu':
 		return 'чт';
-	case 'Fri':
+		case 'Fri':
 		return 'пт';
-    case 'Sat':
+		case 'Sat':
 		return 'сб';
-	case 'Sun':
+		case 'Sun':
 		return 'нд';
 	}
 }
 
 function getMonth(number){
 	switch (number) {
-	case 'Jan':
+		case 'Jan':
 		return 'січня';
-	case 'Feb':
+		case 'Feb':
 		return 'лютого';
-	case 'Mar':
+		case 'Mar':
 		return 'березня';
-	case 'Apr':
+		case 'Apr':
 		return 'квітня';
-	case 'May':
+		case 'May':
 		return 'травня';
-	case 'Jun':
+		case 'Jun':
 		return 'червня';
-	case 'Jul':
+		case 'Jul':
 		return 'липня';
-	case 'Aug':
+		case 'Aug':
 		return 'серпня';
-	case 'Sep':
+		case 'Sep':
 		return 'вересня';
-	case 'Oct':
+		case 'Oct':
 		return 'жовтня';
-	case 'Nov':
+		case 'Nov':
 		return 'листопада';
-	case 'Dec':
+		case 'Dec':
 		return 'грудня';
-    }
+	}
 }
 
 
